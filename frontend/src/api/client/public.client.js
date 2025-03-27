56ac2214b7cd3acecdd12f8d3e2634e3aa88312d
dnnguyen35 <dnnguyeen2003@gmail.com>
2025-03-27 10:21:13 +0700

    set fe axios public url to be

--- frontend/src/api/client/public.client.js
+++ frontend/src/api/client/public.client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 import store from "../../redux/store";
 
-const baseURL = "http://127.0.0.1:5000/api/v1";
+const baseURL = "https://mern-movie-trailer.onrender.com/api/v1";
 
 const publicClient = axios.create({
   baseURL,
